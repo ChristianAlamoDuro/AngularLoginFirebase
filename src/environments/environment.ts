00f6fc3ccf97c258f9bf0948a8cd36cd00346c36
Christian Alamo <christian.alamoduro@outlook.es>
2019-02-24 11:29:25 +0100

    Realizada la configuración de Firebase e instalacion de los modulos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyB8Wcsmdq8Pu7jzE8kvT8sdE6SbJ-vOL2E",
+    authDomain: "loginangular-f21ca.firebaseapp.com",
+    databaseURL: "https://loginangular-f21ca.firebaseio.com",
+    projectId: "loginangular-f21ca",
+    storageBucket: "loginangular-f21ca.appspot.com",
+    messagingSenderId: "1007027913337"
+  }
 };
 
 /*
